348ab70e41710e33e4552e6d147c901cd361b21f
Dmitry Bashkatov <dbashkatov@gmail.com>
2022-05-20 17:08:33 +0300

    add yaml comment

--- kube_manifest_processor/yaml.py
+++ kube_manifest_processor/yaml.py
@@ -1,5 +1,7 @@
 from ruamel.yaml import YAML
 
+# see https://github.com/kubernetes/kubernetes/issues/34146#issuecomment-680825790
+
 
 def my_represent_none(self, data):
     return self.represent_scalar('tag:yaml.org,2002:null', 'null')
