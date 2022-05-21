938bc9ee82fb91255018b8f8f6772bb66ad7d467
Dmitry Bashkatov <dbashkatov@gmail.com>
2022-05-21 03:22:03 +0300

    add remove_prefix filter

--- kube_manifest_processor/filter/simple.py
+++ kube_manifest_processor/filter/simple.py
@@ -1,6 +1,7 @@
 import io
 import json
 import subprocess
+import collections
 
 from .base import Filter
 from ..yaml import YAML
@@ -45,3 +46,20 @@ class External(Filter, name='external'):
 
         result = subprocess.run([self.command], input=marshalled, capture_output=True, check=True)
         return YAML().load(result.stdout)
+
+
+class RemovePrefix(Filter, name='remove_prefix'):
+    def __init__(self, prefix):
+        self.prefix = prefix
+
+    def process(self, obj):
+        if isinstance(obj, collections.MutableMapping):
+            for k, v in obj.items():
+                if k == 'name' and isinstance(v, str):
+                    obj[k] = obj[k].removeprefix(self.prefix)
+                else:
+                    self.process(v)
+        elif isinstance(obj, list):
+            for v in obj:
+                self.process(v)
+        return obj
