42fb7bd0d8460566785e00f955af805f9be72be4
Dmitry Bashkatov <dbashkatov@gmail.com>
2022-05-30 13:35:27 +0300

    add some tests

---
+++ tests/test_yaml.py
@@ -0,0 +1,25 @@
+import os
+
+from kube_manifest_processor import yaml
+
+
+def load_file(name, bytes=False):
+    path = os.path.join(os.path.dirname(__file__), name)
+    with open(path, 'rb' if bytes else 'r') as fp:
+        return fp.read()
+
+
+def test_roundtrip():
+    original = load_file('test1.yaml')
+    assert yaml.dumps(yaml.load(original)) == original
+
+
+def test_multiline():
+    assert yaml.dumps({
+        'key': 'line1\nline2\nline3\n',
+    }) == """
+    key: |
+      line1
+      line2
+      line3
+    """
